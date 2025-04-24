790fad7577784277134b9a91af729c864d917dbd
SamuelZhaoY <samuel.zhao.yue@live.com>
2025-04-24 21:21:08 +0800

    chore: update

--- service/routes.py
+++ service/routes.py
@@ -22,6 +22,7 @@ from flask import jsonify, request, abort
 from flask import url_for  # noqa: F401 pylint: disable=unused-import
 from service.models import Product
 from service.common import status  # HTTP Status Codes
+from service.models import Product, Category
 from . import app
 
 
@@ -99,13 +100,39 @@ def create_products():
 ######################################################################
 
 
+######################################################################
+# LIST PRODUCTS
+######################################################################
 @app.route("/products", methods=["GET"])
 def list_products():
-    """Returns all of the Products"""
-    app.logger.info("Request for product list")
-    products = Product.all()
+    """Returns a list of Products"""
+    app.logger.info("Request to list Products...")
+
+    products = []
+    name = request.args.get("name")
+    category = request.args.get("category")
+    available = request.args.get("available")
+
+    if name:
+        app.logger.info("Find by name: %s", name)
+        products = Product.find_by_name(name)
+    elif category:
+        app.logger.info("Find by category: %s", category)
+        # create enum from string
+        category_value = getattr(Category, category.upper())
+        products = Product.find_by_category(category_value)
+    elif available:
+        app.logger.info("Find by available: %s", available)
+        # create bool from string
+        available_value = available.lower() in ["true", "yes", "1"]
+        products = Product.find_by_availability(available_value)
+    else:
+        app.logger.info("Find all")
+        products = Product.all()
+
     results = [product.serialize() for product in products]
-    return jsonify(results), status.HTTP_200_OK
+    app.logger.info("[%s] Products returned", len(results))
+    return results, status.HTTP_200_OK
 
 ######################################################################
 # R E A D   A   P R O D U C T
@@ -128,17 +155,19 @@ def get_products(product_id):
 
 @app.route("/products/<int:product_id>", methods=["PUT"])
 def update_products(product_id):
-    """Updates a Product"""
-    app.logger.info("Request to update product with id: %s", product_id)
+    """
+    Update a Product
+    This endpoint will update a Product based the body that is posted
+    """
+    app.logger.info("Request to Update a product with id [%s]", product_id)
     check_content_type("application/json")
     product = Product.find(product_id)
     if not product:
-        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' not found.")
-
-    data = request.get_json()
-    product.deserialize(data)
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+    product.deserialize(request.get_json())
+    product.id = product_id
     product.update()
-    return jsonify(product.serialize()), status.HTTP_200_OK
+    return product.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # D E L E T E   A   P R O D U C T
@@ -147,8 +176,11 @@ def update_products(product_id):
 
 @app.route("/products/<int:product_id>", methods=["DELETE"])
 def delete_products(product_id):
-    """Deletes a Product"""
-    app.logger.info("Request to delete product with id: %s", product_id)
+    """
+    Delete a Product
+    This endpoint will delete a Product based the id specified in the path
+    """
+    app.logger.info("Request to Delete a product with id [%s]", product_id)
     product = Product.find(product_id)
     if product:
         product.delete()
